2829fbee190fb950ab45c77451f2f7d00860d11b
Maya the bee <hello@unw.re>
2026-08-25 20:17:20 +0200

    homes/bee: set-up syncthing

--- homes/bee.nix
+++ homes/bee.nix
@@ -30,6 +30,25 @@
     (pkgs.proxmark3.override {withBlueshark = true;})
   ];
 
+  services.syncthing = {
+    enable = true;
+    tray.enable = true;
+
+    settings = {
+      options.urAccepted = -1;
+
+      devices."hypnos.unw.re".id = "5AL4FWV-ZNRXL2O-ZCUOADE-7EUKI5G-WZF2MPA-MGVIPK2-KYH3SHT-F6XSJQJ";
+      folders."~/Library" = {
+        devices = ["hypnos.unw.re"];
+
+        versioning = {
+          type = "simple";
+          params.keep = "10";
+        };
+      };
+    };
+  };
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "24.11"; # Did you read the comment?
 }
